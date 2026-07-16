1fda51a6687b6f70904a7fea5ebceb1c0e953e31
juanmax2 <juanmajge@gmail.com>
2026-07-16 13:56:57 +0200

    Update

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -35,6 +35,5 @@ if settings.DEBUG:
 if settings.DEBUG:
     urlpatterns += [path("__reload__/", include("django_browser_reload.urls"))]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
-else:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
